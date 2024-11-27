18d336cfede1e2c40c56ba732e417c8ba1e9cf8a
macbook <macbook@MacBook-Air-de-macbook.local>
2024-11-27 22:39:45 +0100

    Prosit10

---
+++ src/tn/esprit/gestionDepartements/DepartementHashSet.java
@@ -0,0 +1,60 @@
+package tn.esprit.gestionDepartements;
+
+import java.util.HashSet;
+import java.util.TreeSet;
+import java.util.TreeSet;
+
+public class DepartementHashSet implements IDépartement<Departement>{
+
+    private HashSet<Departement> departements;
+
+    public DepartementHashSet() {
+        departements = new HashSet<>();
+    }
+
+    @Override
+    public void ajouterDepartement(Departement departement) {
+        departements.add(departement);
+    }
+
+    @Override
+    public boolean rechercherDepartement(String nom) {
+        for (Departement departement : departements) {
+            {
+                if (departement.getNomDept().equalsIgnoreCase(nom))
+                    return true; // Département trouvé
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean rechercherDepartement(Departement departement) {
+        return departements.contains(departement);
+    }
+
+    @Override
+    public void supprimerDepartement(Departement departement) {
+        if (departements.remove(departement)) {
+            System.out.println("Département supprimé : " + departement);
+        } else {
+            System.out.println("Département non trouvé : " + departement);
+        }
+    }
+
+    @Override
+    public void displayDepartement() {
+        for (Departement d : departements) {
+            System.out.println(d);
+        }
+    }
+
+    @Override
+    public TreeSet<Departement> trierDepartementById() {
+        TreeSet<Departement> sortedDepartments = new TreeSet<>(
+                (d1, d2) -> Integer.compare(d1.getId(), d2.getId())
+        );
+        sortedDepartments.addAll(departements);
+        return sortedDepartments; // Retourne un TreeSet trié par ID
+    }
+}
